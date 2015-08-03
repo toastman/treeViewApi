14d515117858eee765f9513d60a3e2b40d8590f5
Andrii Vandakurov <andriy.vandakurov@gmail.com>
2015-08-04 02:42:32 +0300

    Work in progress

--- server.js
+++ server.js
@@ -63,8 +63,10 @@ app.get('/', function (req, res) {
   query.treeView ? res.json(resArr) : res.json(flatTree);
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server = app.listen(server_port, function () {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var server = app.listen(server_port, server_ip_address, function () {
   var host = server.address().address;
   var port = server.address().port;
 
